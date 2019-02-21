8a227d1ab41b4fac1588f5c0dc80033bf7d8ca76
Todd Rosenkrantz <todd.rosenkrantz@gmail.com>
2019-02-20 20:43:18 -0600

    Added IEXTrading API basics needs more work but returns Quote information for multiple Symbols

---
+++ app/src/main/java/com/example/WealthMan/APIInterface.java
@@ -0,0 +1,183 @@
+package com.example.WealthMan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.annotations.SerializedName;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+
+public class APIInterface {
+
+    public class Quote{
+        public String symbol = null;
+        public String companyName = null;
+        public String primaryExchange = null;
+        public String sector = null;
+        public String calculationPrice = null;
+        public float open = 0;
+        public long openTime = 0;              // Epoch time
+        public float close = 0;
+        public long closeTime = 0;
+        public float high = 0;
+        public float low = 0;
+        public float latestPrice = 0;
+        public String latestSource = null;
+        public String latestTime = null;
+        public long latestUpdate= 0;           // Epoch time
+        public long latestVolume = 0;
+        public float iexRealtimePrice = 0;
+        public long iexRealtimeSize = 0;
+        public long  iexLastUpdated = 0;       // Epoch time
+        public float delayedPrice = 0;
+        public long  delayedPriceTime = 0;              // Epoch time
+        public float extendedPrice = 0;
+        public float extendedChange = 0;
+        public float extendedChangePercent = 0;
+        public long extendedPriceTime = 0;              // Epoch time
+        public float previousClose = 0;
+        public float change = 0;
+        public float changePercent = 0;
+        public float iexMarketPercent = 0;
+        public long  iexVolume = 0;
+        public long  avgTotalVolume = 0;
+        public float iexBidPrice = 0;
+        public long  iexBidSize = 0;
+        public float iexAskPrice = 0;
+        public long  iexAskSize = 0;
+        public long  marketCap = 0;
+        public float peRatio = 0;
+        public float week52High = 0;
+        public float week52Low = 0;
+        public float ytdChange = 0;
+        public String getSymbol() {
+            return symbol;
+        }
+    }
+    public class article {
+        public String datetime = null;
+        public String headline = null;
+        public String source = null;
+        public String url = null;
+        public String summary = null;
+        public String related = null;
+        public String imageUrl = null;
+    }
+    public class chartDatum{
+        public String date = null; //": "2017-04-03",
+        public String open = null; //": 143.1192,
+        public float high = 0; //": 143.5275,
+        public float low = 0; //": 142.4619,
+        public float close = 0;//": 143.1092,
+        public long volume = 0; //": 19985714,
+        public float unadjustedClose = 0; //": 143.7,
+        public long unadjustedVolume = 0; //": 19985714,
+        public float change = 0; //": 0.039835,
+        public float changePercent = 0; //": 0.028,
+        public float vwap = 0; //": 143.0507,
+        public String label = null; //": "Apr 03, 17",
+        public float changeOverTime = 0; //": -0.0039
+    }
+    public class chart {
+        public List<chartDatum> chartdata = new ArrayList<chartDatum>();
+    }
+    public class news {
+        List<article> articles = new ArrayList<article>();
+    }
+    public class marketQuote{
+        public String itemType = null;
+        public Quote iQuote;
+
+    }
+    public class market{
+        public String mSymbol;
+        public Quote mQuote;
+//        public news mNews;
+//        public chart mChart;
+    }
+    public class results {
+        List<Company> companies = new ArrayList<Company>();
+    }
+    public class Company{
+        public Quote quote;
+    }
+    public static class Companies{
+        List<Company> companies;
+        public Companies(List<Company> companies) {
+            this.companies = companies;
+        }
+
+    }
+    public class stockSym{
+        String symbol;
+        String name;
+        String date;
+        boolean isEnabled;
+        String type;
+        int iexId;
+    }
+    public static class QuoteList {
+        List<Quote> quotes;
+        public QuoteList(List<Quote> quotes) {
+            this.quotes = quotes;
+        }
+
+    }
+    public static class CompanyListDeserializer implements JsonDeserializer<Companies>{
+        @Override
+        public Companies deserialize(JsonElement element, Type type,JsonDeserializationContext context) throws JsonParseException {
+            JsonObject jsonObject = element.getAsJsonObject();
+            List<Company> companies = new ArrayList<Company>();
+            for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+                Company company = context.deserialize(entry.getValue(), Company.class);
+                companies.add(company);
+            }
+            return new Companies(companies);
+        }
+    }
+    public static class QuoteListDeserializer implements JsonDeserializer<QuoteList> {
+
+        @Override
+        public QuoteList deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
+            JsonObject jsonObject = element.getAsJsonObject();
+            List<Quote> quotes = new ArrayList<Quote>();
+            for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+                // For individual Quote objects, we can use default deserialisation:
+                Quote quote = context.deserialize(entry.getValue(), Quote.class);
+                quotes.add(quote);
+            }
+            return new QuoteList(quotes);
+        }
+
+
+    }
+    JsonParser parser = new JsonParser();
+    public String returnJson(String inputString){
+        JsonElement jsonTree = parser.parse(inputString);
+        if (jsonTree.isJsonObject()){
+            JsonObject jsonObject = jsonTree.getAsJsonObject();
+        }
+        return "Jason Data Done";
+//        return outputData;
+    }
+}
--- app/src/main/java/com/example/WealthMan/HomeActivity.java
+++ app/src/main/java/com/example/WealthMan/HomeActivity.java
@@ -1,13 +1,135 @@
 package com.example.WealthMan;
+/*
+Small test to see what is needed to use API to get stock information
 
+https://iextrading.com/developer/docs/#batch-requests
+next steps:
+    parse JSON
+    display needed data
+    create chart from JSON data
+    improve input
+    FIX crash when stock symbol is bad
+
+ */
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import com.example.WealthMan.APIInterface.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.example.WealthMan.APIInterface;
+
 
 public class HomeActivity extends AppCompatActivity {
+    EditText mTextURI;
+    Button mButtonOk;
+//        TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mButtonOk = (Button)findViewById(R.id.button);
+        mTextURI = (EditText)findViewById(R.id.url_to_fetch);
+        final TextView mTextView = (TextView)findViewById(R.id.text);
+        mTextURI.append("");
+        final RequestQueue queue = Volley.newRequestQueue(this);
+
+        mTextView.setMovementMethod(new ScrollingMovementMethod());
+// Gson
+        final GsonBuilder gsonBuilder = new GsonBuilder();
+// ...
+        mButtonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String symbols = mTextURI.getText().toString().trim();
+                // Instantiate the RequestQueue.
+//                String url ="https://api.iextrading.com/1.0/stock/market/batch?symbols="+ symbols +"&types=chart&range=1m&last=5";
+//                String url ="https://api.iextrading.com/1.0/stock/market/batch?symbols="+ symbols +"&types=quote,news,chart&range=6m";
+                String url ="https://api.iextrading.com/1.0/stock/market/batch?symbols="+ symbols +"&types=quote";
+//          Sql query should be like SELECT GROUP_CONCAT(symbol SEPARATOR ',')
+//              or SELECT GROUP_CONCAT(symbol)
+
+//                String url ="https://api.iextrading.com/1.0/stock/"+ symbols +"/quote";
+// Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+//                                mTextView.setText("Response is: "+ response.substring(0,1000));
+                                if (response.equals("{}"))
+                                    mTextView.setText(mTextView.getText() + ("No Data returned.  Did you enter a valid stock symbol?"));
+                                else {
+                                    mTextView.setText("");
+                                    gsonBuilder.registerTypeAdapter(Companies.class, new CompanyListDeserializer());
+                                    Companies myData = gsonBuilder.create().fromJson(response, Companies.class);
+                                    Gson gsonPretty = new GsonBuilder().setPrettyPrinting().create();
+                                    String json = gsonPretty.toJson(myData);
+
+//                                    System.out.println("JSON = " + json);
+//                                    mTextView.setText(json);
+                                    for (int index = 0; index < myData.companies.size(); index++) {
+                                        mTextView.append(Integer.toString(index));
+                                        mTextView.append("\t");
+                                        mTextView.append(myData.companies.get(index).quote.symbol);
+                                        mTextView.append("\t");
+                                        mTextView.append(Float.toString(myData.companies.get(index).quote.latestPrice));
+                                        mTextView.append("\t");
+                                        if(myData.companies.get(index).quote.change < 0){
+                                            mTextView.setTextColor(Color.parseColor("#00FF00"));
+                                            mTextView.append(Float.toString(myData.companies.get(index).quote.change));
+                                        } else {
+                                            mTextView.setTextColor(Color.parseColor("#00FFFF"));
+                                            mTextView.append(Float.toString(myData.companies.get(index).quote.change));
+                                        }
+                                        mTextView.append("\n");
+                                        //mTextView.setTextColor(Color.parseColor("#000000"));
+                                    }
+                                    //mTextView.setText("Response is: " + response);
+
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mTextView.setText("That didn't work! Do you have an internet connection?");
+                    }
+                });
+
+// Add the request to the RequestQueue.
+                queue.add(stringRequest);
+            }
+        });
+    }
+    public void getData(String jsonData) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+//        QuoteList myData = gsonBuilder.create().fromJson(jsonData, QuoteList.class);
+        gsonBuilder.registerTypeAdapter(Companies.class, new CompanyListDeserializer());
+        Companies myData = gsonBuilder.create().fromJson(jsonData, Companies.class);
+        Gson gsonPretty = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(myData);
+        String json = gsonPretty.toJson(myData);
+        System.out.println("JSON = " + json);
+//        int index = 0;
+        for (int index = 0; index < myData.companies.size(); index++) {
+            System.out.println(index + "\t" + myData.companies.get(index).quote.symbol + "\t" + myData.companies.get(index).quote.latestPrice + "\t" + myData.companies.get(index).quote.change);
+        }
     }
 }
