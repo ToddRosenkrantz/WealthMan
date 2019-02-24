d3836138be07c58c07381300151f6cf4097e061f
kong song <392081135@qq.com>
2019-02-24 13:19:38 +0800

    detail content

---
+++ app/src/main/java/com/example/WealthMan/detail/view/DiscountView.java
@@ -0,0 +1,22 @@
+package com.example.WealthMan.detail.view;
+
+
+import android.content.Context;
+
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DiscountView extends View {
+    public DiscountView(Context context) {
+        super(context);
+    }
+
+    public DiscountView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DiscountView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
