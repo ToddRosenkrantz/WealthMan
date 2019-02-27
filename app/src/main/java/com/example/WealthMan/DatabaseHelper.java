4ee934fb72d1aacdab4c2cfb19ff73c07b9790cc
Todd Rosenkrantz <todd.rosenkrantz@gmail.com>
2019-02-27 17:23:54 -0600

    ok
    ok

--- app/src/main/java/com/example/WealthMan/DatabaseHelper.java
+++ app/src/main/java/com/example/WealthMan/DatabaseHelper.java
@@ -27,7 +27,6 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String SYM_COL_3 = "symbol";
 
     public static final String WL_TBL = "watchlist";
-    public static final String WL_COL_USER = "userid";
     public static final String WL_COL_SYMBOL = "symbol";
 
     public DatabaseHelper(Context context) {
@@ -38,7 +37,7 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("CREATE TABLE registeruser (ID INTEGER PRIMARY  KEY AUTOINCREMENT, username TEXT, password TEXT, email TEXT, pin TEXT)");
         sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS symbols (ID INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT, symbol TEXT)");
-        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS watchlist (userid INTEGER, symbol TEXT)");
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS watchlist (symbol TEXT PRIMARY KEY)");
     }
 
     @Override
@@ -53,22 +52,25 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         long res = 0;
         for (int i = 0 ; i < wlArray.length; i++){
             ContentValues contentValues = new ContentValues();
+<<<<<<< HEAD
             contentValues.put(WL_COL_USER, 1);
+=======
+>>>>>>> 8454a95f265dbc6889488890f75749c31d99a143
             contentValues.put(WL_COL_SYMBOL, wlArray[i]);
             res = db.insert(WL_TBL,null,contentValues);
         }
         db.close();
         return res;
     }
-    public long addWatch(String symbol, int userid){
+    public long addWatch(String symbol){
         SQLiteDatabase db = this.getReadableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(WL_COL_USER, userid);
         contentValues.put(WL_COL_SYMBOL, symbol);
         long res = db.insert(WL_TBL,null,contentValues);
         db.close();
         return res;
     }
+<<<<<<< HEAD
     public void remWatch(String symbol, int userid){
         SQLiteDatabase db = this.getReadableDatabase();
         db.execSQL("DELETE FROM " + TABLE_NAME+ " WHERE "+ WL_COL_SYMBOL + " = " + symbol + "AND userid=" + userid +";");
@@ -91,6 +93,14 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         return  userid;
     }
 
+=======
+
+    public void remWatch(String symbol){
+        SQLiteDatabase db = this.getReadableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_NAME+ " WHERE "+WL_COL_SYMBOL+"='"+symbol+"'");
+        db.close();
+    }
+>>>>>>> 8454a95f265dbc6889488890f75749c31d99a143
     
     public String getWatchList(){
         String result;
